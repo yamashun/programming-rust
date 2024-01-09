b598a25171370f10c1708d23a49c1a13fe93c2c5
yamashun <yshunske@gmail.com>
2024-01-09 21:58:22 +0900

    cargo clean

--- hello/target/debug/deps/hello-c9774b34a6e1eca3.d
+++
@@ -1,5 +0,0 @@
-/Users/yamadashunsuke/projects/work/programming-rust/hello/target/debug/deps/hello-c9774b34a6e1eca3: src/main.rs
-
-/Users/yamadashunsuke/projects/work/programming-rust/hello/target/debug/deps/hello-c9774b34a6e1eca3.d: src/main.rs
-
-src/main.rs:
--- hello/target/debug/hello.d
+++
@@ -1,1 +0,0 @@
-/Users/yamadashunsuke/projects/work/programming-rust/hello/target/debug/hello: /Users/yamadashunsuke/projects/work/programming-rust/hello/src/main.rs
